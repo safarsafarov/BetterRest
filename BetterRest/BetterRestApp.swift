d5c34f0ec1a2e108562dfc039d32ba2525349400
safarsafarov <safarov.safar@hotmail.com>
2021-07-13 10:47:28 +0800

    commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Safar Safarov on 2021/8/27.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
